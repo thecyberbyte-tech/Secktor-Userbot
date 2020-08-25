d43642e8e0c0b865b8db42849d17577ca10a7b1b
the cyberbyte <66154594+thecyberbyte@users.noreply.github.com>
2020-08-25 13:36:50 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -18,7 +18,7 @@ IS_SELECTED_DIFFERENT_BRANCH = (
 OFFICIAL_UPSTREAM_REPO = "https://github.com/thecyberbyte/secktor"
 BOT_IS_UP_TO_DATE = "secktor userbot is up-to-date!"
 NEW_BOT_UP_DATE_FOUND = (
-    "**Friday Update Found For** {branch_name}\n"
+    "**Secktor Update Found For** {branch_name}\n"
     "\n\n{changelog}\n"
     "Pulling Updates !!"
 )
