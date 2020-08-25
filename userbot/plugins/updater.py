3ee5bfb8b6183fa4bd8e011a37fddcf9720368aa
the cyberbyte <66154594+thecyberbyte@users.noreply.github.com>
2020-08-25 13:36:15 +0530

    Updated

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -15,8 +15,8 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://github.com/StarkGang/FridayUserbot"
-BOT_IS_UP_TO_DATE = "Friday userbot is up-to-date!"
+OFFICIAL_UPSTREAM_REPO = "https://github.com/thecyberbyte/secktor"
+BOT_IS_UP_TO_DATE = "secktor userbot is up-to-date!"
 NEW_BOT_UP_DATE_FOUND = (
     "**Friday Update Found For** {branch_name}\n"
     "\n\n{changelog}\n"
